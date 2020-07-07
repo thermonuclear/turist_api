aaad45f3bd7afa45f30170010ab87256bbf71130
Anatoliy Smilik <cvbkbr@gmail.com>
2020-07-07 14:54:58 +0500

    add laravel-ide-helper

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // laravel-ide-helper
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
