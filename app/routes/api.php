bab683f6d71bba9527d3c800a91b2b8e90693e57
Anatoliy Smilik <cvbkbr@gmail.com>
2020-07-25 19:49:06 +0500

    add subdomain to api routes

--- app/routes/api.php
+++ app/routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::domain('{account}.' . env('APP_HOST'))->middleware('auth:api')->group(function () {
+
+    Route::match(['get', 'post'], '/add-lead', function (Request $request) {
+        return $request->user();
+    });
+
+
 });
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
